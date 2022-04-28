97c8af6482aa9376c751db260602712b006df4a2
Ajay <abairagi@flair-solution.com>
2022-04-28 11:25:38 +0530

    Added json data

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 const express = require('express');
+const fs = require('fs');
+const data = fs.readFileSync('./json/Employees.json');
+const elements = JSON.parse(data);
+const app = express();
+const cors = require('cors');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.use(cors())
+
+app.use('/employees', allData);
+function allData(request, response) {
+    response.send(elements);
+}
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+})
