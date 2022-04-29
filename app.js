a19b1aa0d4e2a9c8e49a781f932e790d84e988db
Ajay <abairagi@flair-solution.com>
2022-04-29 18:28:24 +0530

    new api for small data

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const fs = require('fs');
-const data = fs.readFileSync('./json/Employee.json');
-const elements = JSON.parse(data);
+const data1 = fs.readFileSync('./json/Employee1.json');
+const data2 = fs.readFileSync('./json/Employee2.json');
+const elements1 = JSON.parse(data1);
+const elements2 = JSON.parse(data2);
 const app = express();
 const cors = require('cors');
 
@@ -10,9 +12,14 @@ const port = process.env.PORT || 3000;
 app.use(express.static('public'));
 app.use(cors())
 
+app.use('/employees-small', allData);
+function allData(request, response) {
+    response.send(elements1);
+}
+
 app.use('/employees', allData);
 function allData(request, response) {
-    response.send(elements);
+    response.send(elements2);
 }
 
 
